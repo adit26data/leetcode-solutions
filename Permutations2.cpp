d51631e3f3f5db97084a0dd0014103b618ece80a
Adit Kaushal <kaushaladit26@gmail.com>
2023-09-23 20:17:43 +0530

    Update Permutations2.cpp

--- Permutations2.cpp
+++ Permutations2.cpp
@@ -63,3 +63,62 @@ public:
 //         return result;
 //     }
 // };
+
+
+// static bool     _foo = ios::sync_with_stdio(false);
+// static ostream* _bar = cin.tie(NULL);
+// class Solution {
+// public:
+//     void permute(vector<vector<int>>&ans, vector<int>&nums, int pos, int size)
+//     {
+//        if(pos==size-1)
+//        {
+//            ans.push_back(nums);
+//            return;
+//        } 
+//        for(int i=pos;i<size;i++)
+//        {
+//            if(i != pos && nums[i] == nums[pos]) continue;
+//            swap(nums[pos],nums[i]);
+//            permute(ans,nums,pos+1,size);
+//        }
+//     }
+//     vector<vector<int>> permuteUnique(vector<int>& nums) 
+//     {
+//         vector<vector<int>>ans;
+//         sort(nums.begin(), nums.end());
+//         int pos=0;
+//         permute(ans,nums,pos,nums.size());
+//         // sort(ans.begin(), ans.end());
+//         // ans.erase(unique(ans.begin(), ans.end()), ans.end());
+//         return ans;
+        
+//     }
+// };
+
+// class Solution {
+// public:
+//     void recursion(vector<int> nums, int i, int n, vector<vector<int> > &result) {
+//         if(i == n-1){
+//             result.push_back(nums);
+//             return;
+//         }
+
+//         for(int k = i; k < n; k++){
+//             if(k != i && nums[k] == nums[i]) continue;
+
+//             swap(nums[k], nums[i]);
+//             recursion(nums, i+1, n, result);
+//         }
+//     }
+//     vector<vector<int> > permuteUnique(vector<int> &nums) {
+//         sort(nums.begin(), nums.end());
+
+//         int n = nums.size();
+
+//         vector<vector<int>> result;
+//         recursion(nums, 0, n, result);
+        
+//         return result;
+//     }
+// };
