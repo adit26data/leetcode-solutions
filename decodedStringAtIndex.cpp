a9deff6245eb004089946ade01a104590a894c07
Adit Kaushal <kaushaladit26@gmail.com>
2023-09-27 09:22:52 +0530

    Create decodedStringAtIndex.cpp

---
+++ decodedStringAtIndex.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string decodeAtIndex(string s, int k) 
+    {
+        long long size=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(isdigit(s[i]))
+               size=size*(s[i]-'0');
+            else
+              size+=1;
+        }
+        for(int i=s.size()-1;i>=0;i--)
+        {
+            k=k%size;
+            if(k==0 && isalpha(s[i]))
+               return string(1,s[i]);
+            if(isdigit(s[i]))
+               size/=(s[i]-'0');
+            else
+               size--;
+        }
+    return "";
+    }
+};
